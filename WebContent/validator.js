ee8c556d0576585b49558d711d9743bc4d2320fb
Espen <espen.norvang@gmail.com>
2019-10-25 00:25:14 +0200

    implementerte en fungerende validator

--- WebContent/validator.js
+++ WebContent/validator.js
@@ -6,12 +6,48 @@ let passordRepetert = document.getElementById("passordRepetert")
 let kjonn = document.getElementById("kjonn")
 let form = document.getElementById("form")
 
-
 fornavn.addEventListener("keyup", function() {
-    let fnavn = fornavn.value;
-    if(fnavn.length < 3){
+    let regex = /^[A-ZÆØÅ][A-ZÆØÅa-zæøå -]{2,19}/;
+    validerFelt(fornavn.value, regex);
+});
+
+etternavn.addEventListener("keyup", function() {
+    let regex = /^[A-ZÆØÅ][A-ZÆØÅa-zæøå-]{2,19}/;
+    validerFelt(etternavn.value, regex);
+});
+
+mobil.addEventListener("keyup", function() {
+    let regex = /[0-9]{8}/;
+    validerFelt(mobil.value, regex);
+});
+
+passord.addEventListener("keyup", function() {
+    validerPassord(passord.value);
+});
+
+passordRepetert.addEventListener("keyup", function() {
+    if(passordRepetert.value !== passord.value){
         event.target.style.borderColor="red";
-    }else if(fnavn.length >=3){
+    }else{
         event.target.style.borderColor="green";
     }
 });
+
+function validerFelt(input, regex){
+    if(!regex.test(input)){
+        event.target.style.borderColor="red";
+    }else{
+        event.target.style.borderColor="green";
+    }
+}
+
+function validerPassord(input){
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if(!regex.test(input)){
+        event.target.style.borderColor="red";
+    }else if(regex.test(input) && input.length < 10){
+        event.target.style.borderColor="yellow";
+    }else{
+        event.target.style.borderColor="green";
+    }
+}
